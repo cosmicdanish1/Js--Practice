9ff8df8900075059ec519e36fc2bc79a8703f378
Danish Qureshi <danishqureshi@gmail.com>
2024-09-02 01:38:45 +0530

    v 1.1.2

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+  const points = [40, 100, 1, 5, 25, 10];
+  let x =  5;
+  for(var i = 0; i< points.length; i++){
+    if(x === points[i]){
+
+      return points[i]
+    }
+
+    }
+
+    return -1
+  }
+
+
+
+  return (
+    
+      <></>
+  )
+}
+
+export default App
